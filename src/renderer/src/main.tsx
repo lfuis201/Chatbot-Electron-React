5a025f92de9c010737714c500ad4f7d6572cf05f
lfuis201 <lmamania@ulasalle.edu.pe>
2025-08-17 23:46:00 -0500

    feat: add react query

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,12 +3,21 @@ import './assets/main.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HeroUIProvider } from '@heroui/react'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+
+const queryClient = new QueryClient()
+
+
 import AppRouter from './routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HeroUIProvider>
-      <AppRouter /> 
-    </HeroUIProvider>{' '}
+       <QueryClientProvider client={queryClient}>
+        <AppRouter />
+      </QueryClientProvider>
+    </HeroUIProvider>
   </StrictMode>
 )
